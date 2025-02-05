803e1734ef7b891ebf4f0ed0ec53fc29f100a614
mingwei <mingwei5087@gmail.com>
2025-02-05 22:02:39 +0800

    CDCNT-8 - Create register API

---
+++ codeconnect-user-management-service/src/main/java/com/nus/iss/appuser/controller/AppUserController.java
@@ -0,0 +1,24 @@
+package com.nus.iss.appuser.controller;
+
+import com.nus.iss.appuser.entity.AppUser;
+import com.nus.iss.appuser.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AppUserController {
+    private final AppUserService appUserService;
+
+    @Autowired
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AppUser> register(@RequestBody AppUser appUser) {
+        AppUser registeredUser = appUserService.registerUser(appUser);
+        return ResponseEntity.ok(registeredUser);
+    }
+}
