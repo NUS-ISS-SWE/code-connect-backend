35b15e1062d9b8e6bb9cb91430d7d455121dd0a1
mingwei <mingwei5087@gmail.com>
2025-02-06 19:58:17 +0800

    CDCNT-8 - Add the unit test

--- codeconnect-user-management-service/src/main/java/com/nus/iss/appuser/service/AppUserService.java
+++ codeconnect-user-management-service/src/main/java/com/nus/iss/appuser/service/AppUserService.java
@@ -2,8 +2,6 @@ package com.nus.iss.appuser.service;
 
 import com.nus.iss.appuser.entity.AppUser;
 
-import java.util.List;
-
 public interface AppUserService {
     AppUser registerUser(AppUser appUser);
 }
