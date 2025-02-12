e6e445f8d47cff07a9626ef6ffed97ed52408677
Yasin Mangera <e1509789@u.nus.edu>
2025-02-12 21:59:29 +0800

    Added CORS handling to help debug

--- codeconnect-profile-management-service/src/main/java/com/nus/iss/controller/ProfileController.java
+++ codeconnect-profile-management-service/src/main/java/com/nus/iss/controller/ProfileController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/profiles")
 public class ProfileController {
