a7f2347b44b91589d1dfd167c58920134f8d2041
SvethaMahadevan <e0665108@u.nus.edu>
2025-03-07 23:23:24 +0800

    Added fetch profile endpoint based on token

--- codeconnect-profile-management-service/src/main/java/com/nus/iss/repository/ProfileRepository.java
+++ codeconnect-profile-management-service/src/main/java/com/nus/iss/repository/ProfileRepository.java
@@ -1,11 +1,13 @@
 package com.nus.iss;
 
+import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
@@ -34,4 +36,6 @@ public interface ProfileRepository extends JpaRepository<Profile, Long> {
         byte[] resumeContent = fileStorageService.getResumeFile(profile.getResumeFileName());
         return new Profile.ResumeData(new String(resumeContent), profile.getResumeFileName());
     }
+
+    Optional<Profile> findByUsername(String username);
 }
