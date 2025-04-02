c340cc951bb32f9c826fc352961807898dc26a25
mingwei <mingwei5087@gmail.com>
2025-04-02 21:42:19 +0800

    CDCND-135 - Update path

--- codeconnect-job-management-service/src/main/java/com/nus/iss/controller/JobPostingController.java
+++ codeconnect-job-management-service/src/main/java/com/nus/iss/controller/JobPostingController.java
@@ -1,5 +1,7 @@
-package com.nus.iss;
+package com.nus.iss.controller;
 
+import com.nus.iss.model.JobPosting;
+import com.nus.iss.service.JobPostingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
