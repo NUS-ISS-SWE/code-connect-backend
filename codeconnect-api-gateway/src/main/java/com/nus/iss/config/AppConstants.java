994fbe965f7187cf584831279f38a366d400763a
mingwei <mingwei5087@gmail.com>
2025-04-04 19:42:40 +0800

    CDCND-139 - Create gateway service

---
+++ codeconnect-api-gateway/src/main/java/com/nus/iss/config/AppConstants.java
@@ -0,0 +1,34 @@
+package com.nus.iss.config;
+
+public class AppConstants {
+    private AppConstants() {
+    }
+    // 8080 for gateway, 8081 for liquibase
+
+    // User service URL
+    public final static String USER_SERVICE_URL = "http://localhost:8082";
+    public final static String USER_ROUTE_SWAGGER = "/user-service/v3/api-docs";
+    public final static String USER_ROUTE_REGISTER = "/api/v1/register";
+    public final static String USER_ROUTE_ACTIVATE = "/api/v1/activate";
+    public final static String USER_ROUTE_LOGIN = "/api/v1/login";
+    public final static String USER_ROUTE_FORGOT_PASSWORD = "/api/v1/forgot-password";
+    public final static String USER_ROUTE_COMPLIANCE_DATA = "/api/v1/compliance-data";
+
+    // Admin service URL
+    public final static String ADMIN_SERVICE_URL = "http://localhost:8083";
+    public final static String ADMIN_ROUTE_SWAGGER = "/admin-service/v3/api-docs";
+    public final static String ADMIN_ROUTE_EMPLOYERS = "/api/v1/employers";
+    public final static String ADMIN_ROUTE_REVIEW_EMPLOYERS = "/api/v1/employers/review/**";
+    public final static String ADMIN_ROUTE_EMPLOYEES = "/api/v1/employees";
+
+    // Job service URL
+    public final static String JOB_SERVICE_URL = "http://localhost:8084";
+    public final static String JOB_ROUTE_SWAGGER = "/job-service/v3/api-docs";
+
+
+    // Constants
+    public final static String EMPLOYER = "EMPLOYER";
+    public final static String EMPLOYEE = "EMPLOYEE";
+    public final static String ADMIN = "ADMIN";
+
+}
---
+++ codeconnect-api-gateway/src/main/java/com/nus/iss/config/GatewayConfig.java
@@ -0,0 +1,60 @@
+package com.nus.iss.config;
+
+import com.nus.iss.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import static com.nus.iss.config.AppConstants.*;
+
+@Configuration
+public class GatewayConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    public GatewayConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Swagger API doc routes (just for aggregation)
+                .route("user-docs", r -> r.path(USER_ROUTE_SWAGGER)
+                        .uri(USER_SERVICE_URL))
+                .route("admin-docs", r -> r.path(ADMIN_ROUTE_SWAGGER)
+                        .uri(ADMIN_SERVICE_URL))
+                .route("job-docs", r -> r.path(JOB_ROUTE_SWAGGER)
+                        .uri(JOB_SERVICE_URL))
+
+                // ==== USER SERVICE ROUTES (localhost:8081) ====
+                .route("user_register", r -> r.path(USER_ROUTE_REGISTER)
+                        .uri(USER_SERVICE_URL))
+                .route("user_activate", r -> r.path(USER_ROUTE_ACTIVATE)
+                        .uri(USER_SERVICE_URL))
+                .route("user_login", r -> r.path(USER_ROUTE_LOGIN)
+                        .uri(USER_SERVICE_URL))
+                .route("compliance_data", r -> r.path(USER_ROUTE_COMPLIANCE_DATA)
+                        .uri(USER_SERVICE_URL))
+
+                // ==== ADMIN SERVICE ROUTES (localhost:8082) ====
+                .route("admin_get_employers", r -> r.path(ADMIN_ROUTE_EMPLOYERS)
+                        .filters(f -> f.filter(jwtAuthenticationFilter.jwtRoleFilter(List.of(ADMIN))))
+                        .uri(ADMIN_SERVICE_URL))
+                .route("admin_review_employer", r -> r.path(ADMIN_ROUTE_REVIEW_EMPLOYERS)
+                        .filters(f -> f.filter(jwtAuthenticationFilter.jwtRoleFilter(List.of(ADMIN))))
+                        .uri(ADMIN_SERVICE_URL))
+                .route("admin_get_employees", r -> r.path(ADMIN_ROUTE_EMPLOYEES)
+                        .filters(f -> f.filter(jwtAuthenticationFilter.jwtRoleFilter(List.of(ADMIN))))
+                        .uri(ADMIN_SERVICE_URL))
+
+                // ==== JOB SERVICE ROUTES (localhost:8083) ====
+
+                .build();
+    }
+}
