f45c07a4cb260447475c2c6ad1ef4ef0d8db6e25
SvethaMahadevan <e0665108@u.nus.edu>
2025-04-06 22:50:58 +0800

    added more details for notif email

--- codeconnect-job-management-service/src/main/java/com/nus/iss/service/JobApplicationService.java
+++ codeconnect-job-management-service/src/main/java/com/nus/iss/service/JobApplicationService.java
@@ -48,6 +48,7 @@ public class JobApplicationService {
         Optional<JobApplication> optionalApplication = jobApplicationRepository.findById(applicationId);
         if (optionalApplication.isPresent()) {
             JobApplication application = optionalApplication.get();
+            JobPosting jobPosting = application.getJobPosting(); // Get associated job posting
 
             // Update the status
             application.setStatus(newStatus);
@@ -56,8 +57,11 @@ public class JobApplicationService {
             // Send email notification
             String subject = "Your Job Application Status Has Changed";
             String body = "Dear " + application.getApplicantName() + ",\n\n"
-                    + "Your application status has been updated to: " + newStatus + ".\n\n"
-                    + "Thank you for applying.";
+                    + "Your application for the position of \"" + jobPosting.getJobTitle() + "\" at " + jobPosting.getCompanyName()
+                    + " has been updated to: " + newStatus + ".\n\n"
+                    + "Job Description: " + jobPosting.getJobDescription() + "\n"
+                    + "Location: " + jobPosting.getJobLocation() + "\n"
+                    + "Thank you for applying to " + jobPosting.getCompanyName() + ".";
             emailService.sendEmail(application.getApplicantEmail(), subject, body);
         } else {
             throw new RuntimeException("Application not found with ID: " + applicationId);
