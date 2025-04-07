41a32a7c9e1c01efcb939e11253289babb288952
mingwei <mingwei5087@gmail.com>
2025-04-07 22:14:52 +0800

    CDCND-135 - Update path in codeconnect-interview-prep-service

--- codeconnect-interview-prep-service/src/main/java/com/nus/iss/model/SkillsAssessment.java
+++ codeconnect-interview-prep-service/src/main/java/com/nus/iss/model/SkillsAssessment.java
@@ -1,12 +1,8 @@
-package com.nus.iss;
+package com.nus.iss.model;
+
+import jakarta.persistence.*;
 
 import java.util.List;
-
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
 public class SkillsAssessment {
--- codeconnect-interview-prep-service/src/test/java/com/nus/iss/service/SkillsAssessmentServiceTest.java
+++ codeconnect-interview-prep-service/src/test/java/com/nus/iss/service/SkillsAssessmentServiceTest.java
@@ -1,4 +1,4 @@
-package com.nus.iss;
+package com.nus.iss.service;
 
 import java.util.Arrays;
 import java.util.List;
