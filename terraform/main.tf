f592c0a01e5e3cdd79dc1ee58152d05a905c467a
mingwei <mingwei5087@gmail.com>
2025-04-24 22:33:49 +0800

    CDCND-99 - Add IAM role

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,40 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+resource "aws_iam_role" "ec2_s3_access_role" {
+  name = "EC2S3AccessRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_read_only_policy" {
+  role       = aws_iam_role.ec2_s3_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_s3_access_profile" {
+  name = "ec2-s3-access-profile"
+  role = aws_iam_role.ec2_s3_access_role.name
+}
+
+
 resource "aws_security_group" "app_server_security_group" {
   name        = "app-server-security-group"
-  description = "Allow SSH, HTTP and 8080 traffic"
+  description = "Allow SSH, HTTP, and 8080 traffic"
   tags = {
     Name = "terraform-ec2-server-security-group"
   }
+
   ingress {
     from_port = 22
     to_port   = 22
@@ -49,14 +77,35 @@ resource "aws_security_group" "app_server_security_group" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-05ab12222a9f39021"
-  instance_type = "t2.micro"
-  key_name      = "code-connect-key-pair"
+  ami                  = "ami-05ab12222a9f39021"
+  instance_type        = "t2.large"
+  key_name             = "code-connect-key-pair"
   security_groups = [aws_security_group.app_server_security_group.name]
+  iam_instance_profile = aws_iam_instance_profile.ec2_s3_access_profile.name
   root_block_device {
     volume_size = 15
     volume_type = "gp3"
   }
+
+  user_data = <<-EOF
+#!/bin/bash
+# Update and install Docker
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+aws s3 cp s3://code-connect-s3/docker-compose.yml /home/ec2-user/docker-compose.yml
+cd /home/ec2-user
+sudo docker-compose up -d
+
+EOF
+
   tags = {
     Name = "terraform-base-main-ec2-server"
   }
