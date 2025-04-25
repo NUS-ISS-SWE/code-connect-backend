a8a5ed51d025e33f43048ae72fb33757f0fcd55c
mingwei <mingwei5087@gmail.com>
2025-04-25 19:10:46 +0800

    CDCND-99 - Add load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = var.region
 }
 
 resource "aws_iam_role" "ec2_s3_access_role" {
@@ -38,7 +38,6 @@ resource "aws_iam_instance_profile" "ec2_s3_access_profile" {
   name = "ec2-s3-access-profile"
   role = aws_iam_role.ec2_s3_access_role.name
 }
-
 
 resource "aws_security_group" "app_server_security_group" {
   name        = "app-server-security-group"
@@ -77,9 +76,9 @@ resource "aws_security_group" "app_server_security_group" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                  = "ami-05ab12222a9f39021"
-  instance_type        = "t2.large"
-  key_name             = "code-connect-key-pair"
+  ami                  = var.iam_image
+  instance_type        = var.instance_type
+  key_name             = var.key_pair
   security_groups = [aws_security_group.app_server_security_group.name]
   iam_instance_profile = aws_iam_instance_profile.ec2_s3_access_profile.name
   root_block_device {
@@ -89,29 +88,24 @@ resource "aws_instance" "app_server" {
 
   user_data = <<-EOF
 #!/bin/bash
-# Update and install Docker
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
-# Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-# Debugging and logging
 echo "Starting Docker Compose..." >> /var/log/user-data.log
 aws s3 cp s3://code-connect-s3/docker-compose.yml /home/ec2-user/docker-compose.yml >> /var/log/user-data.log 2>&1
 cd /home/ec2-user
 
-# Wait for Docker to be ready
 while ! sudo systemctl is-active --quiet docker; do
   echo "Waiting for Docker to start..." >> /var/log/user-data.log
   sleep 20
 done
 
-# Run docker-compose up
 sudo docker-compose up -d >> /var/log/user-data.log 2>&1
 echo "Docker Compose finished running." >> /var/log/user-data.log
 
@@ -120,4 +114,84 @@ EOF
   tags = {
     Name = "terraform-base-main-ec2-server"
   }
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  name        = "load-balancer-security-group"
+  description = "Allow HTTP and HTTPS traffic to the load balancer"
+  tags = {
+    Name = "load-balancer-security-group"
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "app_load_balancer" {
+  name                       = "app-load-balancer"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups = [aws_security_group.load_balancer_security_group.id]
+  subnets                    = var.subnet_ids
+  enable_deletion_protection = false
+  tags = {
+    Name = "app-load-balancer"
+  }
+}
+
+resource "aws_lb_target_group" "app_target_group" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    interval            = 30
+    path                = "/actuator/health"
+    port                = "8080"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+    healthy_threshold   = 2
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "ec2_instance_attachment" {
+  target_group_arn = aws_lb_target_group.app_target_group.arn
+  target_id        = aws_instance.app_server.id
+  port             = 80
+}
+
+output "load_balancer_url" {
+  description = "The URL of the Load Balancer"
+  value       = "http://${aws_lb.app_load_balancer.dns_name}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "subnet_ids" {
+  description = "List of subnet IDs for the ALB"
+  type = list(string)
+  default = ["subnet-0388e7b3c238874d8", "subnet-03f4377e7af006ecb", "subnet-007f0fccf788b15ee"]
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the ALB and EC2 instances are located"
+  type        = string
+  default     = "vpc-0cea0fd7b3c0a6ff8"
+}
+
+variable "region" {
+  description = "The AWS region where resources are deployed"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+  default     = "t2.large"
+}
+
+variable "iam_image" {
+    description = "The AMI ID for the EC2 instance"
+    type        = string
+    default     = "ami-05ab12222a9f39021"
+}
+variable "key_pair" {
+    description = "The name of the key pair to use for SSH access"
+    type        = string
+    default     = "code-connect-key-pair"
+}
