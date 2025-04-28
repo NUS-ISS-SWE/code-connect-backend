fa5bf60c0be61770cf393a5148666c7ea623552b
ShirleyMaX <E1503298@u.nus.edu>
2025-04-28 21:50:05 +0800

    update variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "subnet_ids" {
   description = "List of subnet IDs for the ALB"
   type = list(string)
-  default = ["subnet-0388e7b3c238874d8", "subnet-03f4377e7af006ecb", "subnet-007f0fccf788b15ee"]
+  default = ["subnet-0c80e6a2c951f19ad", "subnet-0ada350d55d95b725", "subnet-09cfd5ea358569994"]
 }
 
 variable "vpc_id" {
   description = "The VPC ID where the ALB and EC2 instances are located"
   type        = string
-  default     = "vpc-0cea0fd7b3c0a6ff8"
+  default     = "vpc-0f8e3dffaac6b9bf5"
 }
 
 variable "region" {
@@ -30,17 +30,17 @@ variable "iam_image" {
 variable "key_pair" {
     description = "The name of the key pair to use for SSH access"
     type        = string
-    default     = "code-connect-key-pair"
+    default     = "jenkins_kp"
 }
 
 variable "s3_docker_compose_source" {
   description = "The path to the private key file for SSH access"
   type        = string
-  default     = "C:\\Users\\mingw\\IdeaProjects\\nus\\code-connect-backend\\docker-compose.yml"
+  default     = "../docker-compose.yml"
 }
 
 variable "alb_target_group_arn" {
   description = "The ARN of the target group for the ALB"
   type        = string
-  default     = "arn:aws:elasticloadbalancing:ap-southeast-1:697982515707:targetgroup/mw-target-group/caef3c2f16c6112c"
+  default     = "arn:aws:elasticloadbalancing:ap-southeast-1:221082174830:targetgroup/CodeConnectTargetGroup/5fe342ba07d17458"
 }
